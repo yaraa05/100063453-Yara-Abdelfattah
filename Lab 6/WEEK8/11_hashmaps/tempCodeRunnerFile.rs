59a6471b6f1601469d230c470d2b55180274a0c6
yaraa05 <y.abdelfattah95@gmail.com>
2025-03-03 09:51:17 +0400

    Add files via upload

---
+++ Lab 6/WEEK8/11_hashmaps/tempCodeRunnerFile.rs
@@ -0,0 +1,5 @@
+let total_fruits: u32 = basket.values().sum();
+    // if total_fruits <= 11 { // Add more fruits if the total is 11 or less
+    //     basket.insert(Fruit::Banana, 2);  
+    //     basket.insert(Fruit::Pineapple, 3); 
+    // }
